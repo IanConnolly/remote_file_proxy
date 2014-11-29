e1358eb317877d38ada7c82ce8c88cd14568035c
Ian Connolly <ian@connolly.io>
2014-11-29 11:51:07 +0000

    Let's not do stuff that makes no sense

--- lib/remote_file_proxy.rb
+++ lib/remote_file_proxy.rb
@@ -2,8 +2,19 @@ require "remote_file_proxy/version"
 
 module RemoteFileProxy
   class RFile
+
+  	UNIMPLEMENTED = [:blockdev?, :chardev?, :chown, :ctime, :expand_path,
+  					 :lchown, :link, :mtime, :pipe?, :realdirpath, :realpath,
+  					 :socket?, :symlink, :utime]
+
     def self.method_missing(method_sym, *args, &block)
       raise NoMethodError unless File.public_methods.include? method_sym
+      raise NotImplementedError if UNIMPLEMENTED.include? method_sym
+
+      # set up connection to the filesystem
+      # find the file
+      # pull it to client disk in temp folder
+      # call File's underlying method, but alter the filename arg
       File.send method_sym, *args, &block
     end
   end
--- test/remote_file_proxy_test.rb
+++ test/remote_file_proxy_test.rb
@@ -5,12 +5,18 @@ require "remote_file_proxy"
 class RFPTest < Minitest::Unit::TestCase
 
   def test_proxy
-    assert RemoteFileProxy::RFile.exists?("test/remote_file_proxy_test.rb")
+    assert RemoteFileProxy::RFile.exists? "test/remote_file_proxy_test.rb"
   end
 
   def test_no_method_on_file
     assert_raises NoMethodError do
       RemoteFileProxy::RFile.methodthatdoesntexist
     end
+  end
+
+  def test_not_implemented
+  	assert_raises NotImplementedError do
+  		RemoteFileProxy::RFile.socket? "test/remote_file_proxy_test.rb"
+  	end
   end
 end
