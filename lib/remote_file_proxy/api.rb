f5535541d7cd49d7e53a89786484689dbc5ab97a
Ian Connolly <ian@connolly.io>
2014-12-16 14:57:30 +0000

    API tests + fix

--- lib/remote_file_proxy/api.rb
+++ lib/remote_file_proxy/api.rb
@@ -7,28 +7,29 @@ module RemoteFileProxy
 
   class API
     def initialize(host, port)
-      @server = TCPSocket.new host, port
+      @server = TCPServer.new host, port
     end
 
     def get_file(filename, folder)
       message = "REQUEST FILENAME=#{filename}\n"
 
-      header = s.gets
+      header = @server.gets
 
       # Response is: FILE CONTENT-LENGTH=[len-in-bytes]\n[file]
       # or: ERROR MESSAGE=[msg]\n
 
       if header.split()[0] == 'ERROR'
-        raise StandardError, "Error: #{header.split()[1].split('=')[1]}"
+        raise APIError, "Error: #{header.split()[1].split('=')[1]}"
       end
 
       content_length = header.split()[1].split('=')[1]
-      file_contents = s.recv(content_length)
+      file_contents = @server.recv(content_length.to_i)
       
       new_filename = File.join(folder, filename)
-      File.open(new_filename, "w") do |f|
-        f.write file_contents
-      end
+
+      f = File.open(new_filename, "w")
+      f.write file_contents
+      f.close
 
       new_filename
     end
---
+++ test/api_test.rb
@@ -0,0 +1,45 @@
+require 'minitest/unit'
+require "minitest/autorun"
+require 'minitest/pride'
+
+require "remote_file_proxy/api"
+
+class APITest < Minitest::Unit::TestCase
+
+  def setup
+    @mocked_socket = Minitest::Mock.new
+    @file_size = 7
+    @mocked_socket.expect(:recv, 'thisis7B', [@file_size])
+  end
+
+  def test_error
+    @mocked_socket.expect(:gets, "ERROR MESSAGE=test\n", [])
+
+    TCPServer.stub :new, @mocked_socket do
+      api = RemoteFileProxy::API.new "host", 90
+      assert_raises RemoteFileProxy::APIError do
+        api.get_file "notimportant", "/usr"
+      end 
+    end
+  end
+
+  def test_file_recv
+    @mocked_socket.expect(:gets, "FILE CONTENT-LENGTH=#{@file_size}\n", [])
+
+    file = Minitest::Mock.new
+    file.expect(:write, nil, ['thisis7B'])
+    file.expect(:close, nil, [])
+
+
+    TCPServer.stub :new, @mocked_socket do
+      api = RemoteFileProxy::API.new "host", 90
+      File.stub :open, file do
+        new_filename = api.get_file "file", "folder"
+        assert_equal new_filename, File.join("folder", "file")
+        file.verify
+      end
+    end
+  end
+
+end
+
